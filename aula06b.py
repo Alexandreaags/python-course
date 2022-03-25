9679665901a179afb7e3c06eca13c061bc5d3a77
Alexandre Adonai <alexandre.adonai2002@hotmail.com>
2022-03-25 11:43:14 -0300

    Aula 6 concluida

---
+++ aula06b.py
@@ -0,0 +1,15 @@
+n = input('Digite um valor: ')
+
+print('O {} é {}:'.format(n, type(n)))
+
+print('Ele é alphanumérico: ', n.isalnum())
+
+print('Ele é alphabético: ', n.isalpha())
+
+print('Ele está em caixa baixa: ', n.islower())
+
+print('Ele está em caixa alta: ', n.isupper())
+
+print('Ele é decimal: ', n.isdecimal())
+
+
