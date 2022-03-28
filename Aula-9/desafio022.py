2f9987bade12707619bbe0684ba63154c59a18f3
Alexandre Adonai <alexandre.adonai2002@hotmail.com>
2022-03-28 15:01:16 -0300

    Aula 09 concluida

---
+++ Aula-9/desafio022.py
@@ -0,0 +1,8 @@
+
+nome = input('Qual o seu nome: ')
+print(nome.upper())
+print(nome.lower())
+nome.strip() #remove espaços inúteis do início e do final da string
+print(len(nome) - nome.count(' '))
+nome.split()
+print(nome.find(' '))
